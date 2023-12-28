b97faad8bd0d1cd5b880f56cc97a9ea5501de9e2
KUMARANVASANTH <kumarakurubaran944@gmail.com>
2023-12-28 16:03:55 +0530

    Code is updated

--- interface.tf
+++ interface.tf
@@ -3,9 +3,9 @@ resource "azurerm_network_interface" "nic-1" {
   resource_group_name = azurerm_resource_group.resource-group-1.name
   location            = azurerm_resource_group.resource-group-1.location
   ip_configuration {
-    name      = "nic-config"
-    subnet_id = azurerm_subnet.subnet-vnet-1.id
-    public_ip_address_id          = azurerm_public_ip.vnet-1-ip.id
+    name                          = "nic-config"
+    subnet_id                     = azurerm_subnet.subnet-vnet-1.id
+    public_ip_address_id          = azurerm_public_ip.vm-1-ip.id
     private_ip_address_allocation = "Dynamic"
   }
 }
--- ip.tf
+++ ip.tf
@@ -16,3 +16,9 @@ resource "azurerm_public_ip" "vhub-ip" {
   resource_group_name = azurerm_resource_group.vhub.name
   allocation_method   = "Dynamic"
 }
+resource "azurerm_public_ip" "vm-1-ip" {
+  name                = "vm-1-ip"
+  location            = azurerm_resource_group.resource-group-1.location
+  resource_group_name = azurerm_resource_group.resource-group-1.name
+  allocation_method   = "Dynamic"
+}

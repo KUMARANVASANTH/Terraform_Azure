629a44d7720fc6d8d6b3d07b3b3ebda9835a1548
KUMARANVASANTH <kumarakurubaran944@gmail.com>
2023-12-29 17:08:49 +0530

    Code is updated

--- connection.tf
+++ connection.tf
@@ -16,3 +16,21 @@ resource "azurerm_virtual_network_gateway_connection" "vhub-vnet2" {
   peer_virtual_network_gateway_id = azurerm_virtual_network_gateway.vnet-1-gateway.id
   shared_key                      = "vnettovhub"
 }
+resource "azurerm_virtual_network_gateway_connection" "vnet2-vhub" {
+  name                            = "vnet2_to_vhub"
+  location                        = azurerm_resource_group.resource-group-2.location
+  resource_group_name             = azurerm_resource_group.resource-group-2.name
+  type                            = "Vnet2Vnet"
+  virtual_network_gateway_id      = azurerm_virtual_network_gateway.vnet-2-gateway.id
+  peer_virtual_network_gateway_id = azurerm_virtual_network_gateway.vhub.id
+  shared_key                      = "vnet2tovhub"
+}
+resource "azurerm_virtual_network_gateway_connection" "vhub-vnet" {
+  name                            = "vhub_to_vnet2"
+  location                        = azurerm_resource_group.vhub.location
+  resource_group_name             = azurerm_resource_group.vhub.name
+  type                            = "Vnet2Vnet"
+  virtual_network_gateway_id      = azurerm_virtual_network_gateway.vhub.id
+  peer_virtual_network_gateway_id = azurerm_virtual_network_gateway.vnet-2-gateway.id
+  shared_key                      = "vnet2tovhub"
+}
